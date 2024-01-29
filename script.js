6a5854e80bb90c59f4fd2b8621f0152e8231e277
Osirus jack <methanblun@gmail.com>
2024-01-29 19:48:55 +0000

    html separated , element renamed

--- script.js
+++ script.js
@@ -1,21 +1,45 @@
 
 
-function addTask() {
-  let taskInput = document.getElementById('taskInput').value;
-  const tasksList = document.getElementById('tasksList');
 
-  if (taskInput === '') {
+
+// document.addEventListener("DOMContentLoaded", function() {
+function addTask() {
+  let taskInputed = document.getElementById('taskInput').value;
+  const generatedTask = document.getElementById('generatedTask')
+
+  if (taskInputed === '') {
     alert("You must write something!");
   } else {
+     
     const toDoDiv = document.createElement("div");
     const newToDo = document.createElement('li');
 
-    newToDo.innerText = taskInput;
+    newToDo.textContent = taskInputed;
     newToDo.classList.add('todo-item');
     
     toDoDiv.appendChild(newToDo);
-    tasksList.appendChild(toDoDiv); 
+    generatedTask.appendChild(toDoDiv); 
 
-    document.getElementById('taskInput').value = '';  // Effacement du champ d'entrée
+    document.getElementById('taskInputed').value = '';  // Effacement du champ d'entrée
   }
 }
+// })
+// function addTask() {
+//   let taskInput = document.getElementById('taskInput').value;
+//   const tasksList = document.getElementById('tasksList');
+
+//   if (taskInput === '') {
+//     alert("You must write something!");
+//   } else {
+//     const toDoDiv = document.createElement("div");
+//     const newToDo = document.createElement('li');
+
+//     newToDo.innerText = taskInput;
+//     newToDo.classList.add('todo-item');
+    
+//     toDoDiv.appendChild(newToDo);
+//     tasksList.appendChild(toDoDiv); 
+
+//     document.getElementById('taskInput').value = '';  // Effacement du champ d'entrée
+//   }
+// }
